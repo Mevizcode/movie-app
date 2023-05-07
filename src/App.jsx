6e7aa772b615cc68b56ca3f8595ecd908fa8ca6e
mevizcode <mevizdaven@gmail.com>
2023-05-07 16:43:04 +0800

    Add new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,65 @@
 import './App.css';
 import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
+import { useEffect, useState }  from 'react';
+const apiUrl = process.env.REACT_APP_API_URL;
 
+// Main Component
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+// Initializes the component to display movies with the title "Avatar"
+useEffect(() => {
+    searchMovies('Avatar');
+}, [])
+
+const searchMovies = async (title) => {
+    await fetch(`${apiUrl}&s=${title}`)
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data);
+        setMovies(data.Search);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+}
+
   return (
-    <div className="App">
+    <div className="app">
       <h1>Movie WebApp</h1>
 
       {/* Search bar */}
       <div className="search">
         <input 
           placeholder="Search for movies"
-          value="Avatar"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img 
-          src=""
+          src={SearchIcon}
           alt="search"
-          onClick={() => {}}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      {/* container for movieCard Component */}
-      <div className="container">
-          <MovieCard />
-      </div>
+      {/*  check & render a list of MovieCard */}
+      {
+        movies?.length > 0 
+        ? (
+         <div className="container">
+          {
+            movies.map((movie) => (
+              <MovieCard movie={movie} />
+          ))}
+         </div>
+        ) : (
+          <div className="empty">
+              <h2>No movies found</h2>
+          </div>
+        )
+      }
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
     return (
         <div className="movie">
             <div>
-
+                <p>{movie.Year}</p>
             </div>
-            <img src="" alt="" />
+            
             <div>
-                <span>{}</span>
-                <h3>Movie Title</h3>
+                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+            </div>
+
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
             </div>
         </div>
     );
